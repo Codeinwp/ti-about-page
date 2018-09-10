b3abadb4d7268e40acbfa6ad723e0db97dac2d76
Andrei Baicus <baicusandrei@me.com>
2018-09-10 16:33:30 +0300

    Add basic files.

---
+++ load.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author:          Andrei Baicus <andrei@themeisle.com>
+ * Created on:      10/09/2018
+ *
+ * @package ti-about-page
+ */
+
+require_once 'class-ti-about-page.php';
+require_once 'includes' . DIRECTORY_SEPARATOR . 'class-plugin-install.php';
